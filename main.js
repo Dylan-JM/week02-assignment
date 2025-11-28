617efeea8bbdeee776ae34dce8db8761d3bbfcfb
Dylan Marley <dylanjmarley01@gmail.com>
2025-11-28 10:14:06 +0000

    feat: added image data

---
+++ main.js
@@ -0,0 +1,20 @@
+//TODO: Create thumnbnail images, so the user can click on them and see them in a fullscreen format
+console.log("Hello, World");
+
+const imageData = [
+  {
+    imageName: "belugaWhale",
+    imageSRC: "./images/bWhale.jpg",
+    imageAlt: "a beluga whale looking above water",
+  },
+  {
+    imageName: "penguin",
+    imageSRC: "./images/penguin.jpg",
+    imageAlt: "A penguin",
+  },
+  {
+    imageName: "redPanda",
+    imageSRC: "./images/redPanda.jpg",
+    imageAlt: "Red panda on a tree",
+  },
+];
